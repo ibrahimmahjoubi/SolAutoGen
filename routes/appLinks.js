c91153806e440383f7128723cc84cc0ae120a8ca
ibrahimmahjoubi <mahjoubiibrahim6@gmail.com>
2018-11-21 12:16:51 +0100

    Update appLinks.js

--- routes/appLinks.js
+++ routes/appLinks.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Pn = require('../models/petrinet');
+const codeGen = require('../CodeGenerator');
 
 router.post('/matrix', (req, res, next)=>{
   let newPetriNet = new Pn({
-    pre: req.body.pre
+    pre: req.body.pre,
+    post: req.body.post,
+    places: req.body.places,
+    transitions: req.body.transitions,
+    initialMarking: req.body.initialMarking
   });
 Pn.addPn(newPetriNet, (err, pn)=>{
    if(err){
      res.json({success: false, msg:'failed to register Petri Net'});
+     console.log(err);
    }else{
     res.json({success: true, msg:'Petri Net Registred'});
+    console.log(newPetriNet.pre);
+    codeGen.CG(newPetriNet);
    }
 });
 
