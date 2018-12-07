ac4503cea2a3b0a8c30a656af9743df242727e00
ibrahimmahjoubi <mahjoubiibrahim6@gmail.com>
2018-12-07 10:23:31 +0100

    Update petrinet.js

--- models/petrinet.js
+++ models/petrinet.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose');
 const config = require('../config/database');
+var Schema = mongoose.Schema;
 
+var task = new Schema({ name: String,
+                        public: Boolean,
+                        payable: Boolean });
 // PentriNet Schema
 const petriNetSchema = mongoose.Schema({
 pre: {
-    type: Array,
+    type: [[Number]],
     required: true
 },
 post: {
     type: Array,
-    //required: true
+    required: true
 },
 places: {
-    type: String,
-    //required: true
+    type: Number,
+    required: true
 },
 transitions: {
-    type: String,
-    //required: true
+    type: Number,
+    required: true
 },
 initialMarking: {
-    type: String,
-    //required: true
-}
+    type: Array,
+    required: true
+},
+tasks: Array
 });
 const Pn = module.exports = mongoose.model('Pn', petriNetSchema);
 
 module.exports.addPn = function(newPn, callback){
+    console.log(newPn.pre);
     newPn.save(callback);
+
+    //var pNet = new petriNet();
+    //pNet.pre = newPn.pre; // The setter will be used automatically here.
+    //console.log(newPn.pre+"*/*/*---*/**");
+    //pNet.sayHello(); // Will output 'Hello, my name is Martin, I have ID: id_1'
+    //console.log(pNet.sayHello());
 }
